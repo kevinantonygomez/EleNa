30aac2968907c2f21abc5422bf185a9cf5c65337
Kevin Gomez <kevingomez@KAGs-MacBook-Pro.local>
2023-04-30 10:22:35 -0400

    All basic required elements added to webpage. Currently just placeholders with no functions

--- public_html/js/indexScript.js
+++ public_html/js/indexScript.js
@@ -1,6 +1,36 @@
-// Initializes embedded map. setView([coordinates], zoom level). Coordinates set to Umass (Du Bois Library)
-var map = L.map('map-div').setView([42.390074647680514, -72.52807366888675], 14);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-maxZoom: 19,
-attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+/*
+Embeds map on document load
+Must set API Key below
+Get key from: https://developers.arcgis.com/
+*/
+function initEsri(){
+  const apiKey = "Your_API_KEY"
+  require(["esri/config", "esri/Map", "esri/views/MapView", "esri/widgets/Search"], function(esriConfig, Map, MapView, Search){
+
+    esriConfig.apiKey = apiKey; // Set API Key
+
+    const map = new Map({
+      basemap: "arcgis-navigation"
+    });
+
+    const view = new MapView({
+      map: map,
+      center: [-72.52807366888675,42.390074647680514], // latitude, longitude (set to Umass (Du Bois library))
+      zoom: 12,
+      container: "map-div" // div id of container
+    });
+
+    var searchWidgetStart = new Search({
+      id: "start-search",
+      view: view, // zooms in on searched address on map
+      container: "start-search-div"
+    });
+
+    var searchWidgetStop = new Search({
+      container: "stop-search-div"
+    });
+
+  });
+}
+
+document.body.addEventListener("load", initEsri());
